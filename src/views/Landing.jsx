d508742604df69ba3761ff8225f24ae9be236529
karan <karan@radicali.com>
2020-08-13 00:39:43 +0530

    update landing page buttons

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -10,6 +10,19 @@ export const Landing = () => {
           link="https://sonic.radicali.io/1/jobs/"
         />
         <LandingButtons text="Build a Scraper" link="/scraper" />
+        <LandingButtons text="Metabase" link="http://metabase.radicali.io/" />
+        <LandingButtons
+          text="Kibana"
+          link="http://kibana-production.radicali.io/login?next=%2F"
+        />
+        <LandingButtons
+          text="Zoho"
+          link="https://app.zohocreator.com/shubhankar9/sonic-dev-apac-2019#Section_1"
+        />
+        <LandingButtons
+          text="Sentry"
+          link="https://sentry.radicali.io/radicali/sonic/"
+        />
       </div>
     </div>
   );
