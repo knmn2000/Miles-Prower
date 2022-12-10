5c3ef4d55efc72e7d7d5bd30d66288eca64dc412
karan.mannan <karan.mannan@toddleapp.com>
2022-12-10 13:00:53 +0530

    workaround: commit build

---
+++ build/precache-manifest.d49364893f5906bac147185f53d9ecc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7f874c1c9ad4717edb72c2e6fa3a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a028d2d7ea32362d3ef",
+    "url": "/static/css/main.08693ea1.chunk.css"
+  },
+  {
+    "revision": "8a2561318a01783d9913",
+    "url": "/static/js/2.8082e409.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8082e409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a028d2d7ea32362d3ef",
+    "url": "/static/js/main.abd57b81.chunk.js"
+  },
+  {
+    "revision": "b8584901cb31ad20897d",
+    "url": "/static/js/runtime-main.3fef871a.js"
+  }
+]);
